ff8c44898beb7b5a87f9842912c05485f37b3f77
Rafael Imakawa <rafael.imakawa@looplex.com.br>
2025-02-06 17:04:07 -0300

    refactor: routes moved from webapi to service
    movedto infra webapi
    web api reference added to infra csproj
    skiping if file exists

--- replacer/scripts/service-infra/csproj.mjs
+++ replacer/scripts/service-infra/csproj.mjs
@@ -1,22 +1,27 @@
-import { CommandProcessor } from '../../utils/commandProcessor.mjs';
+import { CommandProcessor } from '../../utils/commandProcessor.mjs'
 
-export const file = 'src/services/academic/Looplex.DotNet.Samples.Academic.Infra/Looplex.DotNet.Samples.Academic.Infra.csproj';
-export const outputFile = '{{PROJECT_PATH}}/services/{{CC MODULE_NAME}}/{{PROJECT_NAMESPACE}}.{{MODULE_NAME}}.Infra/{{PROJECT_NAMESPACE}}.{{MODULE_NAME}}.Infra.csproj';
+export const file = 'src/services/academic/Looplex.DotNet.Samples.Academic.Infra/Looplex.DotNet.Samples.Academic.Infra.csproj'
+export const outputFile = '{{PROJECT_PATH}}/services/{{CC MODULE_NAME}}/{{PROJECT_NAMESPACE}}.{{MODULE_NAME}}.Infra/{{PROJECT_NAMESPACE}}.{{MODULE_NAME}}.Infra.csproj'
 const replaces = [
-{
-  original: 'Looplex.DotNet.Samples',
-  find: /Looplex.DotNet.Samples/g,
-  replace: '\\{{PROJECT_NAMESPACE}}'
-},
-{
-  original: 'Academic',
-  find: /Academic/g,
-  replace: '{{MODULE_NAME}}'
-}
+  {
+    original: '<ProjectReference Include="..\\..\\..\\Looplex.DotNet.WebApi\\Looplex.DotNet.WebApi.csproj" />',
+    find: /<ProjectReference Include="..\\..\\..\\Looplex.DotNet.WebApi\\Looplex.DotNet.WebApi.csproj" \/>/g,
+    replace: '<PackageReference Include="Looplex.DotNet.WebApi" Version="1.2.1" />'
+  },
+  {
+    original: 'Looplex.DotNet.Samples',
+    find: /Looplex.DotNet.Samples/g,
+    replace: '\\{{PROJECT_NAMESPACE}}'
+  },
+  {
+    original: 'Academic',
+    find: /Academic/g,
+    replace: '{{MODULE_NAME}}'
+  }
 ]
-export default (filePath, outputFilePath) => 
+export default (filePath, outputFilePath) =>
   CommandProcessor.process({
     filePath,
     outputFilePath,
     patterns: replaces
-  });
+  })
--- replacer/scripts/service-infra/webapi.academicroutes.mjs
+++ replacer/scripts/service-infra/webapi.academicroutes.mjs
@@ -1,7 +1,7 @@
 import { CommandProcessor } from '../../utils/commandProcessor.mjs'
 
-export const file = 'src/services/academic/Looplex.DotNet.Samples.Academic.Infra/Ioc/AcademicDependencyContainer.cs'
-export const outputFile = '{{PROJECT_PATH}}/services/{{CC MODULE_NAME}}/{{PROJECT_NAMESPACE}}.{{MODULE_NAME}}.Infra/Ioc/{{MODULE_NAME}}DependencyContainer.cs'
+export const file = 'src/services/academic/Looplex.DotNet.Samples.Academic.Infra/WebApi/AcademicRoutes.cs'
+export const outputFile = '{{PROJECT_PATH}}/services/{{CC MODULE_NAME}}/{{PROJECT_NAMESPACE}}.{{MODULE_NAME}}.Infra/WebApi/{{MODULE_NAME}}Routes.cs'
 const replaces = [
   {
     original: 'Students',
@@ -14,16 +14,6 @@ const replaces = [
     replace: '{{RESOURCE_TYPE_NAME}}'
   },
   {
-    original: 'Students',
-    find: /students/g,
-    replace: '{{CC RESOURCE_TYPE_NAME_P}}'
-  },
-  {
-    original: 'Student',
-    find: /student/g,
-    replace: '{{CC RESOURCE_TYPE_NAME}}'
-  },
-  {
     original: 'Looplex.DotNet.Samples',
     find: /Looplex.DotNet.Samples/g,
     replace: '{{PROJECT_NAMESPACE}}'
@@ -32,11 +22,6 @@ const replaces = [
     original: 'Academic',
     find: /Academic/g,
     replace: '{{MODULE_NAME}}'
-  },
-  {
-    original: 'Academic',
-    find: /academic/g,
-    replace: '{{CC MODULE_NAME}}'
   }
 ]
 
